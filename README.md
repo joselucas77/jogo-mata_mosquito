8d27e9f9b9ad4555432ee950f5830ac0c68cfaad
José Lucas <joselucasa937@gmail.com>
2024-01-13 16:09:39 -0300

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# jogo-mata_mosquito
- App Game Mata Mosquito em JavaScript
+# Jogo Mata Mosquito
+
+## App Game Mata Mosquito em JavaScript
+
+O jogo conta com três níveis de dificuldades(normal, difícil e Batman com preparo).
+Seu objetivo é matar o máximo de mosquitos, os quais nascem aleatoriamente na sua tela, sem perder suas 3 vidas. Boa sorte!
+
+<img src="img/gif.gif" alt="visualização no pc">
+
+<a href="https://joselucas77.github.io/jogo-mata_mosquito/">🔗 Acesse aqui o projeto</a>
+
+## Tecnologias ultilizadas e informações adicionais
+
+- HTML
+- CSS
+- Javascript
+- site responsivo
--- js/app.js
+++ js/app.js
@@ -7,11 +7,11 @@ var nivel = window.location.search
 nivel = nivel.replace('?', '')
 var TempoCriarMosquito = 1500
 
-if(nivel === 'normal') {
+if (nivel === 'normal') {
     TempoCriarMosquito = 1500
-} else if(nivel === 'dificil') {
+} else if (nivel === 'dificil') {
     TempoCriarMosquito = 1000
-} else if(nivel === 'batman') {
+} else if (nivel === 'batman') {
     TempoCriarMosquito = 750
 }
 
@@ -19,40 +19,40 @@ function tamanhoPalco() {
     altura = window.innerHeight
     largura = window.innerWidth
 
-    console.log(altura, largura)
+    // console.log(altura, largura)
 }
 
 tamanhoPalco()
 
-var conometro = setInterval(function() {
+var conometro = setInterval(function () {
 
     tempo -= 1
-    if(tempo < 0) {
+    if (tempo < 0) {
         clearInterval(conometro)
         clearInterval(criarMosquito)
         window.location.href = 'you_win.html'
     } else {
         document.getElementById('cronometro').innerHTML = tempo
     }
-    
+
 }, 1000)
 
 //Criar posições randômicas
 function posicaoRandomica() {
 
     //remover o mosquito anterior (caso exista)
-    if(document.getElementById('mosquito')) {
+    if (document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
 
         // Controlando os pontos de vida
-        if(vidas > 3) {
+        if (vidas > 3) {
             window.location.href = 'game_over.html'
         } else {
             document.getElementById('v' + vidas).src = "img/coracao_vazio.png"
 
             vidas++
         }
-        
+
     }
 
 
@@ -64,7 +64,7 @@ function posicaoRandomica() {
     posicaoX = posicaoX < 0 ? 0 : posicaoX
     posicaoY = posicaoY < 0 ? 0 : posicaoY
 
-    console.log(posicaoX, posicaoY)
+    // console.log(posicaoX, posicaoY)
 
     //criar o elemento html
     var mosquito = document.createElement('img')
@@ -75,7 +75,7 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
-    mosquito.onclick = function() {
+    mosquito.onclick = function () {
         this.remove()
     }
 
@@ -85,10 +85,10 @@ function posicaoRandomica() {
 
 //Tamanhos randômicos
 function tamanhoAleatorio() {
-    var classe = Math.floor(Math.random()*3)
+    var classe = Math.floor(Math.random() * 3)
     // console.log(classe)
 
-    switch(classe) {
+    switch (classe) {
         case 0:
             return 'mosquito1'
         case 1:
@@ -100,9 +100,9 @@ function tamanhoAleatorio() {
 
 //Lado A e lado B
 function ladoAleatorio() {
-    var classe = Math.floor(Math.random()*2)
+    var classe = Math.floor(Math.random() * 2)
 
-    switch(classe) {
+    switch (classe) {
         case 0:
             return 'ladoA'
         case 1:
